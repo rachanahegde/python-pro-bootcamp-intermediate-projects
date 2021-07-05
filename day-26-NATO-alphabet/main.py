2636ebd188a6334d7e393a50127daa901e5d9d77
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2021-07-05 21:22:32 +0800

    Add files via upload

---
+++ day-26-NATO-alphabet/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+is_running = True
+
+while is_running:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+    else:
+        is_running = False
+        print(output_list)
