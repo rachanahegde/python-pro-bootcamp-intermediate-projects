0dcebb9ab49a2c2c80d3d634d0ff627e080fcbc2
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2021-07-13 20:53:19 +0800

    Update main.py

--- day-24-mail-merge/main.py
+++ day-24-mail-merge/main.py
@@ -28,3 +28,19 @@ for name in list_names:
 # TODO: Create a letter using starting_letter.txt for each name in invited_names.txt
 # TODO: Replace the [name] placeholder with the actual name.
 # TODO: Save the letters in the folder "ReadyToSend".
+
+# ------------------- Alternative Solution ----------------- #
+# PLACEHOLDER = "[name]"
+
+
+# with open("./Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+
+# with open("./Input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip()
+#         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_letter)
+
